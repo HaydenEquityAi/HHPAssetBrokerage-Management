df82bc6471a9b5872fb6b6053432346e0d7ee157
HaydenEquityAi <hayden@hhpasset.com>
2026-01-10 18:20:01 -0600

    Edit Website

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -110,7 +110,7 @@ const FAQ = () => {
       {/* Hero Section */}
       <section className="relative h-[400px] sm:h-[500px] flex items-center justify-center">
         <img 
-          src="/images/faq-hero.jpg" 
+          src="/images/investment-sales-capital-markets-hero.png" 
           alt="FAQ"
           className="absolute inset-0 w-full h-full object-cover"
         />
