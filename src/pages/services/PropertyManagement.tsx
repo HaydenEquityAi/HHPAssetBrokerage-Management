c4354d1edffa454beb032e7d69c3a5742171739b
HaydenEquityAi <hayden@hhpasset.com>
2026-01-10 23:17:45 -0600

    Edit Website

--- src/pages/services/PropertyManagement.tsx
+++ src/pages/services/PropertyManagement.tsx
@@ -10,7 +10,7 @@ const PropertyManagement = () => {
       {/* Hero Section */}
       <section 
         className="relative min-h-[500px] flex items-center justify-center bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: 'url(/images/property-management-image.jpg)' }}
+        style={{ backgroundImage: 'url(/images/property-management-picture.webp)' }}
       >
         <div className="absolute inset-0 bg-hhp-navy/60"></div>
         <div className="relative z-10 container-premium">
